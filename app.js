a8d7adb7fc466941a40cd12efea56fa0951afc01
Shixue Mai <shixuemai@outlook.com>
2022-06-14 16:32:50 -0400

    Add routing

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const date=require(__dirname+"/date.js");
+const mongoose=require("mongoose");
+const lodash=require("lodash");
 
 const app=express();
-
-const items=["Buy Food","Cook Food","Eat Food"];
-const workitems=[];
-const otheritems=[];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
 
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+
+const itemsSchema={ 
+    name:String
+    };
+
+const Item=mongoose.model("Item", itemsSchema);
+
+const item1=Item({"name":"Welcome to the todo list"});
+const item2=Item({"name":"Hit the + button to add a new item"});
+const item3=Item({"name":"<- Hit this to delete an item"});
+
+const defaultItems=[item1,item2,item3];
+
+const listSchema={
+    name:String,
+    items:[itemsSchema]
+};
+
+const List=mongoose.model("List", listSchema);
+
 app.get("/", function(req,res){
-    
-    let day=date.getDate();
-    res.render("list",{listTitle:day,newListItems:items});
+    Item.find({},function(err,founditems){
+        if(founditems.length===0) {
+            Item.insertMany(defaultItems, function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log("successfully inserted");
+                }
+            })
+            res.redirect('/');
+        } else {
+            res.render("list",{listTitle:"Today",newListItems:founditems});
+        }
+    })
 
 })
 
-app.get("/work", function(req,res){
-    res.render("list",{listTitle:"Work List",newListItems:workitems})
-})
-
-app.get("/others", function(req,res) {
-    res.render("list",{listTitle:"Other List",newListItems:otheritems})
+app.get('/:address',function(req,res){
+    const customeListName=lodash.capitalize(req.params.address);
+    List.findOne({name:customeListName},function(err,foundList){
+        if(!err){
+            if(!foundList){
+                //create a new list
+                const list=new List({
+                    name:customeListName,
+                    items:defaultItems
+                })
+                list.save()
+                res.redirect('/'+customeListName);
+            } else {
+                res.render("list",{listTitle:customeListName,newListItems:foundList.items});
+            }
+        }
+    })
 })
 
 app.get('/about', function(req,res){
@@ -33,19 +74,46 @@ app.get('/about', function(req,res){
 })
 
 app.post("/", function(req,res){
-    if(req.body.list==="Work"){
-        var workitem=req.body.newItem;
-        workitems.push(workitem);
-        res.redirect("/work");
-    } else if(req.body.list==="Other"){
-        var otheritem=req.body.newItem;
-        otheritems.push(otheritem);
-        res.redirect("/others");
+    const itemName=req.body.newItem;
+    const listName=req.body.list;
+
+    const item=Item({"name":itemName});
+
+    if(listName==="Today"){
+        item.save();
+        res.redirect('/');
     } else {
-        var item=req.body.newItem;
-        items.push(item);
-        res.redirect("/")
+        List.findOne({name:listName},function(err,foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect('/'+listName);
+        })
     }
+    
+})
+
+app.post("/delete", function(req,res){
+    const checkedItemId=req.body.checkbox;
+    const listName=req.body.listName;
+
+    if(listName==="Today"){
+        Item.findByIdAndRemove(checkedItemId, function(err){
+            if(err){
+                console.log(err);
+            } else {
+                console.log("successfully deleted");
+            }
+        })
+        res.redirect('/');
+    } else {
+        List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},function(err,foundList){
+            if(!err){
+                res.redirect('/'+listName);
+            }
+        })
+    }
+
+    
 })
 
 app.listen(3000,function(req,res){
